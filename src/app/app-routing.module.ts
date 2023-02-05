0c01bf87fac9a1b07c735ea0c947ee18b623c9ed
kaori <fabianakaori.93@gmail.com>
2023-02-05 02:45:51 -0300

    Add rota

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BooksComponent } from "./pages/books/books.component";
+import { CharactersComponent } from "./pages/characters/characters.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { MoviesComponent } from "./pages/movies/movies.component";
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'filmes', component: MoviesComponent },
+    { path: 'livros', component: BooksComponent },
+    {path: 'personagens', component: CharactersComponent}
+]
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { BooksComponent } from './pages/books/books.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent,
+    BooksComponent,
+    CharactersComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
